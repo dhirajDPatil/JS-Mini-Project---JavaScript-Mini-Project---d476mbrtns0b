bf7cfc90dd470270d039c973393089632a9e4082
Dhiraj Patil <mr.dhirajpatil1995@gmail.com>
2022-07-01 15:50:45 +0530

    Basic Layout of Authentication page

--- App.js
+++ App.js
@@ -1,1 +1,33 @@
-console.log("Hello There");
+function authenticateUserRegistration(ev){
+    ev.preventDefault();
+    var userName = document.getElementById("nameId").value;
+    var userN = document.getElementById("loginId").value;
+    var passW = document.getElementById("currentp").value;
+    localStorageOfCredentials(userN, passW);
+    alert("Successfully Registered...");
+
+}
+
+function localStorageOfCredentials(email, password){
+    localStorage.setItem(email,password);
+}  // stored email and password within local storage
+
+function authenticateUserLogin(e){
+    e.preventDefault();
+    var userN = document.getElementById("loginId").value;
+    var passW = document.getElementById("currentp").value;
+    let userPassWord;
+    try{
+        userPassWord = localStorage.getItem(userN)
+    } catch{
+        alert("No such user exist");
+        return;
+    }
+    if(passW == userPassWord){
+        alert("Authorized");
+        document.getElementById("loginId").value = "";
+        document.getElementById("currentp").value = "";
+    } else{
+        alert("invalid credentials/ password");
+    }
+}
